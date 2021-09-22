9d9baa169fe9ebfb61fc5fce6c6494d2580ffbce
ThomasJamesCrawford <ThomasJamesCrawford@gmail.com>
2021-09-22 22:06:57 +0800

    networking

--- lib/aws-microk8s-stack.ts
+++ lib/aws-microk8s-stack.ts
@@ -1,9 +1,38 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as rds from "@aws-cdk/aws-rds";
+import * as asg from "@aws-cdk/aws-autoscaling";
 
 export class AwsMicrok8SStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "my-vpc", {
+      cidr: "10.0.0.0/16",
+      natGateways: 0,
+      maxAzs: 3,
+      subnetConfiguration: [
+        {
+          name: "public-subnet-1",
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
+        },
+        {
+          name: "private-subnet-1",
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 28,
+        },
+      ],
+    });
+
+    const ec2InstanceSG = new ec2.SecurityGroup(this, "ec2-instance-sg", {
+      vpc,
+    });
+
+    ec2InstanceSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      "allow SSH connections from anywhere"
+    );
   }
 }
