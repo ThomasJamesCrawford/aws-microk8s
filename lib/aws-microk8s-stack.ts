a4292ab04688b503066b0a05c6ac7943dc167578
ThomasJamesCrawford <ThomasJamesCrawford@gmail.com>
2021-09-22 22:17:38 +0800

    asg

--- lib/aws-microk8s-stack.ts
+++ lib/aws-microk8s-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from "@aws-cdk/core";
 import * as ec2 from "@aws-cdk/aws-ec2";
 import * as rds from "@aws-cdk/aws-rds";
 import * as asg from "@aws-cdk/aws-autoscaling";
+import { KeyPair } from "cdk-ec2-key-pair";
 
 export class AwsMicrok8SStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -34,5 +35,38 @@ export class AwsMicrok8SStack extends cdk.Stack {
       ec2.Port.tcp(22),
       "allow SSH connections from anywhere"
     );
+
+    const key = new KeyPair(this, "KeyPair", {
+      name: "cdk-keypair",
+      description: "key pair created by cdk deployment",
+    });
+
+    const autoscalingGroup = new asg.AutoScalingGroup(
+      this,
+      "autoscaling-group",
+      {
+        vpc,
+        vpcSubnets: {
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        securityGroup: ec2InstanceSG,
+        instanceType: ec2.InstanceType.of(
+          ec2.InstanceClass.T3,
+          ec2.InstanceSize.MICRO
+        ),
+        machineImage: new ec2.AmazonLinuxImage({
+          generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+        }),
+        keyName: key.keyPairName,
+        minCapacity: 1,
+      }
+    );
+
+    new cdk.CfnOutput(this, "Key Name", { value: key.keyPairName });
+
+    new cdk.CfnOutput(this, "Download Key Command", {
+      value:
+        "aws secretsmanager get-secret-value --secret-id ec2-ssh-key/cdk-keypair/private --query SecretString --output text > cdk-key.pem && chmod 400 cdk-key.pem",
+    });
   }
 }
