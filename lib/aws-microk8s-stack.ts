96c601623bb8195609dc1634861f2117ee788f38
ThomasJamesCrawford <ThomasJamesCrawford@gmail.com>
2021-09-23 18:20:46 +0800

    dont bother with asg

--- lib/aws-microk8s-stack.ts
+++ lib/aws-microk8s-stack.ts
@@ -1,7 +1,6 @@
 import * as cdk from "@aws-cdk/core";
 import * as ec2 from "@aws-cdk/aws-ec2";
 import * as rds from "@aws-cdk/aws-rds";
-import * as asg from "@aws-cdk/aws-autoscaling";
 import { KeyPair } from "cdk-ec2-key-pair";
 
 export class AwsMicrok8SStack extends cdk.Stack {
@@ -36,54 +35,68 @@ export class AwsMicrok8SStack extends cdk.Stack {
       "allow SSH connections from anywhere"
     );
 
+    ec2InstanceSG.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80));
+
     const key = new KeyPair(this, "KeyPair", {
       name: "cdk-keypair",
       description: "key pair created by cdk deployment",
     });
 
-    const autoscalingGroup = new asg.AutoScalingGroup(
-      this,
-      "autoscaling-group",
-      {
-        vpc,
-        vpcSubnets: {
-          subnetType: ec2.SubnetType.PUBLIC,
-        },
-        securityGroup: ec2InstanceSG,
-        instanceType: ec2.InstanceType.of(
-          ec2.InstanceClass.T3,
-          ec2.InstanceSize.MICRO
-        ),
-        machineImage: new ec2.AmazonLinuxImage({
-          generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-        }),
-        keyName: key.keyPairName,
-        minCapacity: 1,
-      }
-    );
+    const elasticIp = new ec2.CfnEIP(this, "elastic-ip");
 
-    autoscalingGroup.userData.addCommands(
+    const ec2Instance = new ec2.Instance(this, "ec2-instance", {
+      vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      securityGroup: ec2InstanceSG,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T3,
+        ec2.InstanceSize.MICRO
+      ),
+      machineImage: ec2.MachineImage.fromSsmParameter(
+        "/aws/service/canonical/ubuntu/server/focal/stable/current/amd64/hvm/ebs-gp2/ami-id",
+        { os: ec2.OperatingSystemType.LINUX }
+      ),
+      keyName: key.keyPairName,
+      blockDevices: [
+        {
+          deviceName: "/dev/sda1",
+          volume: ec2.BlockDeviceVolume.ebs(20),
+        },
+      ],
+    });
+
+    new ec2.CfnEIPAssociation(this, "ec2EIPAssociation", {
+      eip: elasticIp.ref,
+      instanceId: ec2Instance.instanceId,
+    });
+
+    ec2Instance.userData.addCommands(
+      "apt update",
+      "apt install snapd",
       "snap install microk8s --classic",
-      "usermod -a -G microk8s ec2-user",
-      "chown -f -R ec2-user ~/.kube",
+      "usermod -a -G microk8s ubuntu",
+      "chown -f -R ubuntu ~/.kube",
+      "newgrp microk8s",
       "microk8s status --wait-ready",
       "microk8s enable dns storage helm3"
     );
 
     // This doesn't have termination protection so be careful
-    const rdsInstance = new rds.DatabaseInstance(this, "Cdk-rds-instance", {
-      engine: rds.DatabaseInstanceEngine.postgres({
-        version: rds.PostgresEngineVersion.VER_13_3,
-      }),
-      instanceType: ec2.InstanceType.of(
-        ec2.InstanceClass.T2,
-        ec2.InstanceSize.MICRO
-      ),
-      vpc,
-      vpcSubnets: {
-        subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
-      },
-    });
+    // const rdsInstance = new rds.DatabaseInstance(this, "Cdk-rds-instance", {
+    //   engine: rds.DatabaseInstanceEngine.postgres({
+    //     version: rds.PostgresEngineVersion.VER_12_7,
+    //   }),
+    //   instanceType: ec2.InstanceType.of(
+    //     ec2.InstanceClass.T2,
+    //     ec2.InstanceSize.MICRO
+    //   ),
+    //   vpc,
+    //   vpcSubnets: {
+    //     subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+    //   },
+    // });
 
     new cdk.CfnOutput(this, "Key Name", { value: key.keyPairName });
 
--- test/aws-microk8s.test.ts
+++ test/aws-microk8s.test.ts
@@ -1,13 +1,21 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as AwsMicrok8S from '../lib/aws-microk8s-stack';
+import {
+  expect as expectCDK,
+  matchTemplate,
+  MatchStyle,
+} from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import * as AwsMicrok8S from "../lib/aws-microk8s-stack";
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new AwsMicrok8S.AwsMicrok8SStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test("Empty Stack", () => {
+  const app = new cdk.App();
+  const stack = new AwsMicrok8S.AwsMicrok8SStack(app, "MyTestStack");
+
+  expectCDK(stack).to(
+    matchTemplate(
+      {
+        Resources: {},
+      },
+      MatchStyle.EXACT
+    )
+  );
 });
